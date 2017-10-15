b3e782f8557116fca8532cb69fba27012cff52fe
sayadaazami <sayadaazami@gmail.com>
2017-10-15 13:20:29 +0330

    division functionality and its test,... added

--- phpspec-test/spec/CalculatorSpec.php
+++ phpspec-test/spec/CalculatorSpec.php
@@ -20,4 +20,14 @@ class CalculatorSpec extends ObjectBehavior
 	    $this->add(0, 0)->shouldReturn(0);
 	    $this->add(1, -2)->shouldReturn(-1);
 	}
+
+	function it_can_divide_two_numbers()
+	{
+	    $this->divide(10, 2)->shouldReturn(5);
+	}
+
+	function it_throws_a_division_by_zero_excption()
+	{
+	    $this->shouldThrow('\Predmond\Calculator\DivisionByZeroException')->duringDivide(10, 0);
+	}
 }
--- phpspec-test/src/Calculator.php
+++ phpspec-test/src/Calculator.php
@@ -1,10 +1,20 @@
 <?php
 
 namespace Predmond\Calculator;
+use Predmond\Calculator\DivisionByZeroException;
 
 class Calculator
 {
 	public function add($x, $y){
 		return $x + $y;
+	}
+
+    public function divide($x, $y)
+	{
+		if ($y === 0) {
+	        throw new DivisionByZeroException();
+	    }
+
+	    return $x / $y;
 	}
 }
