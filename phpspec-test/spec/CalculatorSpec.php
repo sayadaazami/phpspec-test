3890a32baf1be743e85c5bc53602f1688231a243
sayadaazami <sayadaazami@gmail.com>
2017-10-15 12:38:29 +0330

    iit

---
+++ phpspec-test/spec/CalculatorSpec.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace spec\Predmond\Calculator;
+
+use Predmond\Calculator\Calculator;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class CalculatorSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(Calculator::class);
+    }
+}
